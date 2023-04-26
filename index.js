982a9de488b11009dfb35dca732fc8266af2f5fd
masakarabala <48476935+masakarabala@users.noreply.github.com>
2023-04-26 18:04:31 +0200

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution in this file!
+ let drivers=['Antonia', 'Nuru', 'Amari', 'Mo'];
+ console.log(drivers);
+
+const returnFirstTwoDrivers =function(drivers) {
+return  drivers.slice(0, 2);
+ }
+ console.log(returnFirstTwoDrivers(drivers));//['Antonia', 'Nuru']
+
+ 
+ 
+ const returnLastTwoDrivers =function(drivers) {
+    return  drivers.slice(2, 4);
+    
+     }
+     console.log(returnLastTwoDrivers(drivers));//['Amari', 'Mo']
+   
+ let selectingDrivers=[returnFirstTwoDrivers ,returnLastTwoDrivers];
+ 
+ 
+function createFareMultiplier (number1) {
+    return function(number2) {
+      return number1*number2;
+    }
+  }
+
+  const fareDoubler=createFareMultiplier(2);
+  
+  const fareTripler=createFareMultiplier(3);
+
+  /*function selectDifferentDrivers(returnFirstTwoDrivers ,returnLastTwoDrivers){
+    returnFirstTwoDrivers();
+    returnLastTwoDrivers();
+  }
+  function test (a,b) { 
+    // ...
+};
+// pass only the second parameter 
+test( ... , b);*/
+function selectDifferentDrivers(drivers ,functions){
+   return functions(drivers)
+  }
+  
